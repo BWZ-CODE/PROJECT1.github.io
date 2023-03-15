ab3c8f624b0793726a70f636e8eee8672208a134
Miguel Angel Bohorquez <mmige24@gmail.com>
2023-03-14 20:53:11 -0500

    Hicimos una pequeña presentación de lo que es TABZ, acompañada con JS, falta ajustarla para que sea responsive

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 //////////CONSTANTES///////////////////
 const mob_button = document.querySelector('.button_mob');
 const mob_menu = document.querySelector('.Mob_menu');
+const presentacion_button  = document.querySelector('.title_init');
+const presentacion = document.querySelector ('.presentacion');
+const close_button = document.querySelector('.close_present')
 
 
 
 
 ///////////ESCUCHA DE EVENTOS////////////
 mob_button.addEventListener('click', activeMobMenu);
+presentacion_button.addEventListener('click', activePresentacion);
+close_button.addEventListener('click', closePresentacion)
 
 
 
-//////////////FUNCIONES/////////////////
+//////////////FUNCION mobile menu/////////////////
 const isContPrClosed = containerinit.classList.toggle('inactive')
 
 if(!isContPrClosed){
@@ -21,6 +26,13 @@ if(!isContPrClosed){
 function activeMobMenu(){
     mob_menu.classList.toggle('inactive');  
 }
-
-
+/////////// funcion presentacion/////////////////
+function activePresentacion(){
+    console.log("sfsfs");
+    presentacion.classList.toggle('inactive');
+}
+/////////////funcion cerrar presentacion////
+function closePresentacion(){
+    presentacion.classList.add('inactive');
+}
 
